f2f3ec72db9ac4b4d5990c776551a3a398678fa2
Mauro Bianchi <bianchimro@gmail.com>
2013-02-18 17:28:25 +0100

    added missing files

---
+++ pyevolve/gtree_utils.py
@@ -0,0 +1,160 @@
+import random
+from gtreenode import GTreeNode
+from gtreenode import GTreeNodeGP
+
+#################################
+#    Tree utilsity Functions     # 
+#################################
+
+
+def buildGTreeGrow(depth, value_callback, max_siblings, max_depth):
+   """ Random generates a Tree structure using the value_callback
+   for data generation and the method "Grow"
+
+   :param depth: the initial depth, zero
+   :param value_callback: the function which generates the random
+                          values for nodes
+   :param max_siblings: the maximum number of sisters of a node
+   :param max_depth: the maximum depth of the tree   
+
+   :rtype: the root node of created tree
+   """
+
+   random_value = value_callback()
+   n = GTreeNode(random_value)
+
+   if depth == max_depth: return n
+
+   for i in xrange(random.randint(0, abs(max_siblings))):
+      child = buildGTreeGrow(depth+1, value_callback, max_siblings, max_depth)
+      child.setParent(n)
+      n.addChild(child)
+   return n
+
+def buildGTreeFull(depth, value_callback, max_siblings, max_depth):
+   """ Random generates a Tree structure using the value_callback
+   for data generation and the method "Full"
+
+   :param depth: the initial depth, zero
+   :param value_callback: the function which generates the random
+                          values for nodes
+   :param max_siblings: the maximum number of sisters of a node
+   :param max_depth: the maximum depth of the tree   
+
+   :rtype: the root node of created tree
+   """
+
+   random_value = value_callback()
+   n = GTreeNode(random_value)
+
+   if depth == max_depth: return n
+
+   if max_siblings < 0: range_val = abs(max_siblings)
+   else:                range_val = random.randint(1, abs(max_siblings))
+ 
+   for i in xrange(range_val):
+      child = buildGTreeFull(depth+1, value_callback, max_siblings, max_depth)
+      child.setParent(n)
+      n.addChild(child)
+   return n
+   
+   
+#################################
+#    Tree GP utilsity Functions  # 
+#################################
+
+def gpdec(**kwds):
+   """ This is a decorator to use with genetic programming non-terminals
+   
+   It currently accepts the attributes: shape, color and representation.
+   """
+   def decorate(f):
+      for k in kwds:
+            setattr(f, k, kwds[k])
+      return f
+   return decorate
+
+def checkTerminal(terminal):
+   """ Do some check on the terminal, to evaluate ephemeral constants
+
+   :param terminal: the terminal string
+   """
+   if terminal.startswith("ephemeral:"):
+      splited = terminal.split(":")
+      ephemeral_constant = eval(splited[1])
+      return str(ephemeral_constant)
+   else:
+      return terminal
+
+def buildGTreeGPGrow(ga_engine, depth, max_depth):
+   """ Creates a new random GTreeGP root node with subtrees using
+   the "Grow" method.
+   
+   :param ga_engine: the GA Core
+   :param depth: the initial depth
+   :max_depth: the maximum depth of the tree
+   :rtype: the root node
+   """
+
+   gp_terminals = ga_engine.get_param("gp_terminals")
+   assert gp_terminals is not None
+
+   gp_function_set = ga_engine.get_param("gp_function_set")
+   assert gp_function_set is not None
+
+   if depth == max_depth:
+      random_terminal = checkTerminal(random.choice(gp_terminals))
+      n = GTreeNodeGP(random_terminal, Consts.nodeType["TERMINAL"])
+      return n
+   else:
+      # Do not generate degenerative trees 
+      if depth == 0:
+         random_node = random.choice(gp_function_set.keys())
+      else:
+         fchoice = random.choice([gp_function_set.keys(), gp_terminals])
+         random_node = random.choice(fchoice)
+
+      if random_node in gp_terminals:
+         n = GTreeNodeGP(checkTerminal(random_node), Consts.nodeType["TERMINAL"])
+      else:
+         n = GTreeNodeGP(random_node, Consts.nodeType["NONTERMINAL"])
+
+   if n.getType() == Consts.nodeType["NONTERMINAL"]:
+      for i in xrange(gp_function_set[n.getData()]):
+         child = buildGTreeGPGrow(ga_engine, depth+1, max_depth)
+         child.setParent(n)
+         n.addChild(child)
+
+   return n
+
+def buildGTreeGPFull(ga_engine, depth, max_depth):
+   """ Creates a new random GTreeGP root node with subtrees using
+   the "Full" method.
+   
+   :param ga_engine: the GA Core
+   :param depth: the initial depth
+   :max_depth: the maximum depth of the tree
+   :rtype: the root node
+   """
+   gp_terminals = ga_engine.get_param("gp_terminals")
+   assert gp_terminals is not None
+
+   gp_function_set = ga_engine.get_param("gp_function_set")
+   assert gp_function_set is not None
+
+   if depth == max_depth:
+      random_terminal = checkTerminal(random.choice(gp_terminals))
+      n = GTreeNodeGP(random_terminal, Consts.nodeType["TERMINAL"])
+      return n
+   else:
+      random_oper = random.choice(gp_function_set.keys())
+      n = GTreeNodeGP(random_oper, Consts.nodeType["NONTERMINAL"])
+
+   if n.getType() == Consts.nodeType["NONTERMINAL"]:
+      for i in xrange(gp_function_set[n.getData()]):
+         child = buildGTreeGPFull(ga_engine, depth+1, max_depth)
+         child.setParent(n)
+         n.addChild(child)
+
+   return n
+
---
+++ pyevolve/gtreenode.py
@@ -0,0 +1,179 @@
+from gtreebase import GTreeNodeBase
+
+class GTreeNode(GTreeNodeBase):
+
+   """ The GTreeNode class - The node representation
+
+   Inheritance diagram for :class:`GTree.GTreeNode`:
+
+   .. inheritance-diagram:: GTree.GTreeNode
+
+   :param data: the root node of the tree
+   :param parent: the parent node, if root, this
+                  must be *None*
+   """
+
+   def __init__(self, data, parent=None):
+      super(GTreeNode, self).__init__(parent)
+      self.node_data = data
+
+   def __repr__(self):
+      str_repr  = GTreeNodeBase.__repr__(self)
+      str_repr += " - [%s]" % self.node_data
+      return str_repr     
+
+   def setData(self, data):
+      """ Sets the data of the node
+
+      :param data: the data of the node
+      """
+      self.node_data = data
+
+   def getData(self):
+      """ Return the data of the node
+
+      :rtype: the data of the node
+      """
+      return self.node_data
+
+   def newNode(self, data):
+      """ Created a new child node
+
+      :param data: the data of the new created node
+      """
+      node = GTreeNode(data, self)
+      self.addChild(node)
+      return node
+
+   def swapNodeData(self, node):
+      """ Swaps the node data with another node
+      
+      :param node: the node to do the data swap
+      """
+      tmp_data = self.node_data
+      self.setData(node.getData())
+      node.setData(tmp_data)
+
+   def copy(self, g):
+      """ Copy the contents to the destination g
+      
+      :param g: the GTreeNode genome destination
+      """
+      super(GTreeNode, self).copy(g)
+      g.node_data = self.node_data
+
+   def clone(self):
+      """ Return a new instance of the genome
+      
+      :rtype: new GTree instance
+      """
+      newcopy = GTreeNode(None)
+      self.copy(newcopy)
+      return newcopy
+
+
+
+      
+      
+class GTreeNodeGP(GTreeNodeBase):
+   """ The GTreeNodeGP Class - The Genetic Programming Node representation
+   
+   Inheritance diagram for :class:`GTree.GTreeNodeGP`:
+
+   .. inheritance-diagram:: GTree.GTreeNodeGP
+
+   :param data: the node data
+   :param type: the node type
+   :param parent: the node parent
+   
+   """
+   def __init__(self, data, node_type=0, parent=None):
+      super(GTreeNodeGP, self) .__init__(parent)
+      self.node_type = node_type
+      self.node_data = data
+
+   def __repr__(self):
+      str_repr  = GTreeNodeBase.__repr__(self)
+      str_repr += " - [%s]" % self.node_data
+      return str_repr     
+
+   def compare(self, other):
+      """ Compare this node with other 
+      
+      :param other: the other GTreeNodeGP
+      """
+      if not isinstance(other, GTreeNodeGP):
+         utils.raise_exception("The other node used to compare is not a GTreeNodeGP class", TypeError)
+
+      if other.node_type == self.node_type:
+         if other.node_data == self.node_data:
+            return 0
+      return -1
+
+   def setData(self, data):
+      """Sets the node internal data
+      
+      :param data: the internal data
+      """
+      self.node_data = data
+
+   def getData(self):
+      """Gets the node internal data
+      
+      :rtype: the internal data
+      """
+      return self.node_data
+
+   def setType(self, node_type):
+      """Sets the node type 
+      
+      :param node_type: the node type is type of Consts.nodeType
+      """
+      self.node_type = node_type
+
+   def getType(self):
+      """Get the node type 
+      
+      :rtype: the node type is type of Consts.nodeType
+      """
+      return self.node_type
+
+   def newNode(self, data):
+      """Creates a new node and adds this
+      node as children of current node
+
+      :param data: the internal node data
+      """
+      node = GTreeNodeGP(data, self)
+      self.addChild(node)
+      return node
+
+   def swapNodeData(self, node):
+      """Swaps the node data and type with another node
+
+      :param node: the node
+      """
+      tmp_data = self.node_data
+      tmp_type = self.node_type
+      self.setData(node.getData())
+      self.setType(node.getType())
+      node.setData(tmp_data)
+      node.setType(tmp_type)
+
+   def copy(self, g):
+      """ Copy the contents to the destination g
+      
+      :param g: the GTreeNodeGP genome destination
+      """
+      super(GTreeNodeGP, self).copy(g)
+      g.node_data = self.node_data
+      g.node_type = self.node_type
+
+   def clone(self):
+      """ Return a new copy of the node
+
+      :rtype: the new GTreeNodeGP instance
+      """
+      newcopy = GTreeNodeGP(None)
+      self.copy(newcopy)
+      return newcopy
