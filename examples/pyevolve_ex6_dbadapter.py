5ba8d8e2e7c4fc260a3ffe3fc9c43a1688a559bf
Mauro Bianchi <bianchimro@gmail.com>
2013-02-19 18:23:09 +0100

    more tests

--- examples/pyevolve_ex6_dbadapter.py
+++ examples/pyevolve_ex6_dbadapter.py
@@ -15,33 +15,38 @@ def eval_func(chromosome):
          score += 0.5
    return score
 
-# Genome instance
-genome = g1dlist.G1DList(100)
-genome.set_params(rangemin=0, rangemax=10)
+def test_run_main():
+    # Genome instance
+    genome = g1dlist.G1DList(100)
+    genome.set_params(rangemin=0, rangemax=10)
+    
+    # The evaluator function (objective function)
+    genome.evaluator.set(eval_func)
+    
+    # Genetic Algorithm Instance
+    ga = GSimpleGA.GSimpleGA(genome, 666)
+    ga.setGenerations(80)
+    ga.setMutationRate(0.2)
+    
+    # Create DB Adapter and set as adapter
+    #sqlite_adapter = DBAdapters.DBSQLite(identify="ex6", resetDB=True)
+    #ga.setDBAdapter(sqlite_adapter)
+    
+    # Using CSV Adapter
+    #csvfile_adapter = DBAdapters.DBFileCSV()
+    #ga.setDBAdapter(csvfile_adapter)
+    
+    # Using the URL Post Adapter
+    # urlpost_adapter = DBAdapters.DBURLPost(url="http://whatismyip.oceanus.ro/server_variables.php", post=False)
+    # ga.setDBAdapter(urlpost_adapter)
+    
+    # Do the evolution, with stats dump
+    # frequency of 10 generations
+    ga.evolve(freq_stats=10)
+    
+    # Best individual
+    #print ga.bestIndividual()
+    
+if __name__ == "__main__":
+   test_run_main()
 
-# The evaluator function (objective function)
-genome.evaluator.set(eval_func)
-
-# Genetic Algorithm Instance
-ga = GSimpleGA.GSimpleGA(genome, 666)
-ga.setGenerations(80)
-ga.setMutationRate(0.2)
-
-# Create DB Adapter and set as adapter
-#sqlite_adapter = DBAdapters.DBSQLite(identify="ex6", resetDB=True)
-#ga.setDBAdapter(sqlite_adapter)
-
-# Using CSV Adapter
-#csvfile_adapter = DBAdapters.DBFileCSV()
-#ga.setDBAdapter(csvfile_adapter)
-
-# Using the URL Post Adapter
-# urlpost_adapter = DBAdapters.DBURLPost(url="http://whatismyip.oceanus.ro/server_variables.php", post=False)
-# ga.setDBAdapter(urlpost_adapter)
-
-# Do the evolution, with stats dump
-# frequency of 10 generations
-ga.evolve(freq_stats=10)
-
-# Best individual
-#print ga.bestIndividual()
--- integration_test/test_all_examples.py
+++ integration_test/test_all_examples.py
@@ -1,6 +1,15 @@
 import examples
 
 from examples.pyevolve_ex1_simple import test_run_main as test_1
+from examples.pyevolve_ex2_realgauss import test_run_main as test_2
+from examples.pyevolve_ex3_schaffer import test_run_main as test_3
+from examples.pyevolve_ex4_sigmatrunc import test_run_main as test_4
+from examples.pyevolve_ex5_callback import test_run_main as test_5
+from examples.pyevolve_ex6_dbadapter import test_run_main as test_6
+from  examples.pyevolve_ex7_rastrigin import test_run_main as test_7
+from examples.pyevolve_ex8_gauss_int import test_run_main as test_8
+from examples.pyevolve_ex9_g2dlist import test_run_main as test_9
+
 from examples.pyevolve_ex10_g1dbinstr import test_run_main as test_10
 from examples.pyevolve_ex11_allele import test_run_main as test_11
 
@@ -15,13 +24,5 @@ from examples.pyevolve_ex17_gtree import test_run_main as test_17
 #import examples.pyevolve_ex20_gp_dotwrite
 #from examples.pyevolve_ex21_nqueens import test_run_main as test_21
 from examples.pyevolve_ex22_monkey import test_run_main as test_22
-from examples.pyevolve_ex2_realgauss import test_run_main as test_2
-import examples.pyevolve_ex3_schaffer
-import examples.pyevolve_ex4_sigmatrunc
-from examples.pyevolve_ex5_callback import test_run_main as test_5
-import examples.pyevolve_ex6_dbadapter
-import examples.pyevolve_ex7_rastrigin
-import examples.pyevolve_ex8_gauss_int
-import examples.pyevolve_ex9_g2dlist
 
 
