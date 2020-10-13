17876f3989266fb874cee7651f00d4164bc067a0
Mauro Bianchi <bianchimro@gmail.com>
2020-10-13 22:57:01 +0200

    archive commit

--- integration_test/test_all_examples.py
+++ integration_test/test_all_examples.py
@@ -1,29 +1,29 @@
-import examples
 
-from examples.pyevolve_ex1_simple import test_run_main as test_1
-from examples.pyevolve_ex2_realgauss import test_run_main as test_2
-from examples.pyevolve_ex3_schaffer import test_run_main as test_3
-from examples.pyevolve_ex4_sigmatrunc import test_run_main as test_4
-from examples.pyevolve_ex5_callback import test_run_main as test_5
 
-from examples.pyevolve_ex6_dbadapter import test_run_main as test_6
-from examples.pyevolve_ex7_rastrigin import test_run_main as test_7
-from examples.pyevolve_ex8_gauss_int import test_run_main as test_8
-from examples.pyevolve_ex9_g2dlist import test_run_main as test_9
-from examples.pyevolve_ex10_g1dbinstr import test_run_main as test_10
+from pyevolve.examples.pyevolve_ex1_simple import test_run_main as test_1
+from pyevolve.examples.pyevolve_ex2_realgauss import test_run_main as test_2
+from pyevolve.examples.pyevolve_ex3_schaffer import test_run_main as test_3
+from pyevolve.examples.pyevolve_ex4_sigmatrunc import test_run_main as test_4
+from pyevolve.examples.pyevolve_ex5_callback import test_run_main as test_5
 
-from examples.pyevolve_ex11_allele import test_run_main as test_11
-#from examples.pyevolve_ex12_tsp import test_run_main as test_12
-from examples.pyevolve_ex13_sphere import test_run_main as test_13
-from examples.pyevolve_ex14_ackley import test_run_main as test_14
-from examples.pyevolve_ex15_rosenbrock import test_run_main as test_15
+from pyevolve.examples.pyevolve_ex6_dbadapter import test_run_main as test_6
+from pyevolve.examples.pyevolve_ex7_rastrigin import test_run_main as test_7
+from pyevolve.examples.pyevolve_ex8_gauss_int import test_run_main as test_8
+from pyevolve.examples.pyevolve_ex9_g2dlist import test_run_main as test_9
+from pyevolve.examples.pyevolve_ex10_g1dbinstr import test_run_main as test_10
 
-from examples.pyevolve_ex16_g2dbinstr import test_run_main as test_16
-from examples.pyevolve_ex17_gtree import test_run_main as test_17
-#from examples.pyevolve_ex18_gp import test_run_main as test_18
-#from examples.pyevolve_ex19_gp import test_run_main as test_19
-#from examples.pyevolve_ex20_gp_dotwrite import test_run_main as test_20
+from pyevolve.examples.pyevolve_ex11_allele import test_run_main as test_11
+#from pyevolve.examples.pyevolve_ex12_tsp import test_run_main as test_12
+from pyevolve.examples.pyevolve_ex13_sphere import test_run_main as test_13
+from pyevolve.examples.pyevolve_ex14_ackley import test_run_main as test_14
+from pyevolve.examples.pyevolve_ex15_rosenbrock import test_run_main as test_15
 
-#from examples.pyevolve_ex21_nqueens import test_run_main as test_21
-from examples.pyevolve_ex22_monkey import test_run_main as test_22
-from examples.pyevolve_ex23_g1dbswapmut import test_run_main as test_23
+from pyevolve.examples.pyevolve_ex16_g2dbinstr import test_run_main as test_16
+from pyevolve.examples.pyevolve_ex17_gtree import test_run_main as test_17
+#from pyevolve.examples.pyevolve_ex18_gp import test_run_main as test_18
+#from pyevolve.examples.pyevolve_ex19_gp import test_run_main as test_19
+#from pyevolve.examples.pyevolve_ex20_gp_dotwrite import test_run_main as test_20
+
+#from pyevolve.examples.pyevolve_ex21_nqueens import test_run_main as test_21
+from pyevolve.examples.pyevolve_ex22_monkey import test_run_main as test_22
+from pyevolve.examples.pyevolve_ex23_g1dbswapmut import test_run_main as test_23
